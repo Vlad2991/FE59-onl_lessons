9065c0dfcb34f3b457959d961abadab80c723d63
Vlad2991 <vlad29911710@gmail.com>
2024-03-27 17:08:43 +0300

    init hw.js

---
+++ hw.js
@@ -0,0 +1,10 @@
+"use strict";
+// *********************Задача1*********************
+const string = 'true';
+const bool = false;
+const number = 17;
+const not = undefined ;
+const nOOl = null;
+console.log(typeof string, bool, number, not, nOOl);
+
+// *********************Задача2*********************
