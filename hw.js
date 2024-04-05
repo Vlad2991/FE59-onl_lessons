60c2af16707ad5d4add01f474dc65c6b9ebc1241
Vlad2991 <vlad29911710@gmail.com>
2024-04-05 22:13:45 +0300

    start task 20

---
+++ hw.js
@@ -0,0 +1,9 @@
+'use strict'
+
+  // ************************Задача1************************
+let fruits = ["plum", "lemon", "cherry", "grapes"];
+console.log(fruits[1],fruits[3]);
+console.log(fruits.length);
+let fruitsNew = fruits.push("apple");
+fruits[2] = "gooseberry";
+console.log(fruits);
