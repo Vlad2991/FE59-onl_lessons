b3a93257dcd31dda22e438643bc174391ddb7b5f
Vlad2991 <vlad29911710@gmail.com>
2024-04-06 22:57:52 +0300

    finish part 1 lesson20

--- hw.js
+++ hw.js
@@ -1,9 +1,53 @@
 'use strict'
 
-  // ************************Задача1************************
-let fruits = ["plum", "lemon", "cherry", "grapes"];
-console.log(fruits[1],fruits[3]);
-console.log(fruits.length);
-let fruitsNew = fruits.push("apple");
-fruits[2] = "gooseberry";
-console.log(fruits);
+// // ************************Задача1************************
+// const colors = ['red', 'green', 'blue']
+// console.log(colors.length)
+
+// // ************************Задача2************************
+// const animals = ['monkey', 'dog', 'cat']
+// console.log(animals[animals.length - 1])
+
+// ************************Задача3************************
+// let numbers = [5, 43, 63, 23, 90];
+// numbers = [];
+// console.log(numbers);
+
+// const numbers = [5, 43, 63, 23, 90];
+// numbers.splice(0, numbers.length);
+// console.log(numbers);
+
+// ************************Задача4************************
+// const students = ['Polina', 'Dasha', 'Masha']
+
+// students.pop(2);
+// students.push("Borya");
+// students.shift();
+// students.unshift("Andrey");
+// console.log(students);
+
+// ************************Задача5************************
+// const cats = ['Gachito', 'Tom', 'Batman'];
+// for (let i = 0; i < cats.length; i++){
+// console.log(cats[i]);
+// }
+
+
+// const cats = ['Gachito', 'Tom', 'Batman'];
+// for (const dogs of cats) {
+// console.log(dogs);
+// }
+
+// ************************Задача6************************
+// const evenNumbers = [2, 4, 6, 8, 10];
+// const oddNumbers = [1, 3, 5, 7, 9];
+
+// const evenOddNumbers = oddNumbers.concat(evenNumbers);
+// console.log(evenOddNumbers);
+// const number8 = evenOddNumbers.indexOf(8);
+// console.log(number8);
+
+// ************************Задача7************************
+const binary = [0, 0, 0, 0]
+const strokBinary = (`0${1}0${1}0${1}0${1} `)
+console.log(typeof(strokBinary) )
