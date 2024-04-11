2dfd2b37390f0609a43cbed03f8d3129749ecb6d
Vlad2991 <vlad29911710@gmail.com>
2024-04-11 14:54:30 +0300

    finish part 1 hw 21

---
+++ hw.js
@@ -0,0 +1,69 @@
+"use strict"
+// *********************Задание1*********************
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+// fibonacci.forEach((number) => console.log(number));
+
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+// function printElement(number) {
+//     console.log(number);
+//   }
+//   fibonacci.forEach(printElement);
+
+// *********************Задание2*********************
+// const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton']
+
+// // function newUser(user, index) {
+// //     return `member ${index + 1}: ${user}`;
+// // }
+
+// const members = users.map(newUser);
+
+// console.log(members);
+// const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton']
+
+// const members = users.map((user1, index1) => `member ${index1 + 1}: ${user1}`);
+
+// console.log(members);
+
+// *********************Задание3*********************
+// const numbers = [7, -4, 32, -90, 54, 32, -21]
+// function delet(number) {
+//     return number >= 0;
+//   }
+
+//   const numbersPlus = numbers.filter(delet);
+
+//   console.log(numbersPlus);
+
+
+//   const numbers = [7, -4, 32, -90, 54, 32, -21]
+//   const numbersPlus = numbers.filter(number => number >= 0);
+//   console.log(numbersPlus);
+
+// *********************Задание4*********************
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+// function sum(van1 , van2){
+//     return van1 + van2;
+
+// }
+// const summOff = fibonacci.reduce(sum, 0)
+// console.log(summOff);
+
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+// const summOff = fibonacci.reduce((van1, van2) => van1 + van2 ,0);
+// console.log(summOff);
+
+// *********************Задание5*********************//
+// const numbers = [5, 9, 13, 24, 54, 10, 13, 99, 1, 5]
+// function finds(value) {
+//     return value % 2 === 0;
+
+// }
+// const findResult = numbers.find(finds)
+// console.log(findResult);
+
+// const numbers = [5, 9, 13, 24, 54, 10, 13, 99, 1, 5]
+// const findResult = numbers.find(value => value %2 ===0);
+// console.log(findResult);
+
+// *********************Задание1 Продвинутый*********************//
