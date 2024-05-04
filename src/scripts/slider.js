06d35eac1590713cb458cccfbc76ac2dfc22de36
Vlad2991 <vlad29911710@gmail.com>
2024-05-04 14:41:29 +0300

    startwildberries

--- src/scripts/slider.js
+++
@@ -1,9 +0,0 @@
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
-
-const swiper = new Swiper({
-    bulletClass: 'slider__dot',
-    nextEl: 'class__name',
-    prevEl: 'class__name',
-});
