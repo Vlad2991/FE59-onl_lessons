d899bac7f0f5256d8534cfc93979610e413c7131
karas.e <karas.e@ifuture.by>
2024-06-11 22:21:05 +0300

    add some fix

--- react-app/src/components/banner/index.jsx
+++ react-app/src/components/banner/index.jsx
@@ -1,32 +1,25 @@
+import React from 'react';
 import {Button} from '../button';
 import styles from './index.css';
 
-// class View {
-//     render() {
-//         return <h1>Banner</h1>
-//     }
-// }
-
-// const Banner = new View();
-
-// export default Banner;
-
-export const Banner = (props) => {
-    return (
-        <section className="banner">
-            <div className="banner__container">
-                <div className="banner__wrapper">
-                    <div className="banner__info">
-                        <h1 className="banner__title">Hello world</h1>
-                        <p className="banner__text">Cool!</p>
-                        <Button title="Get it!" />
-                        {/* <button className="banner__btn">Get it!</button> */}
-                    </div>
-                    <div className="banner__img">
-                        <img src="" alt="" />
+export class Banner extends React.Component {
+    render() {
+        return (
+            <section className="banner">
+                <div className="banner__container">
+                    <div className="banner__wrapper">
+                        <div className="banner__info">
+                            <h1 className="banner__title">Hello world</h1>
+                            <p className="banner__text">Cool!</p>
+                            <Button title="Get it!" />
+                            {/* <button className="banner__btn">Get it!</button> */}
+                        </div>
+                        <div className="banner__img">
+                            <img src="" alt="" />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    );
+            </section>
+        );
+    }
 }
